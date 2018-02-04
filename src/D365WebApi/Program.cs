eeb6c557e94bd2d63f38c8c1438249a3e846f6b0
Sunny Rajwadi <sunny@cloudkestrel.com>
2018-02-04 11:31:46 +0530

    Adding dependency injection and console logger.

--- src/D365WebApi/Program.cs
+++ src/D365WebApi/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+/*
+ * CREDIT: https://github.com/andrewlock/blog-examples/tree/master/using-dependency-injection-in-a-net-core-console-application
+ */
+
 
 namespace D365WebApi
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //    Uncomment to use the built in container
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                //.AddSingleton<IFooService, FooService>()
+                .BuildServiceProvider();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Debug);
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogDebug("Starting application");
+
+            logger.LogDebug("All done!");
         }
     }
 }
