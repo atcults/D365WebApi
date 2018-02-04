c5ba300797fd699c28cca443e1cc3d77f7f49c4d
Sunny Rajwadi <sunny@cloudkestrel.com>
2018-02-04 10:53:55 +0530

    Creating .net core console.

---
+++ D365WebApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D365WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
